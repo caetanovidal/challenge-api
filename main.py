ee074225e80f994062fc0b237d701d92f70c5dc0
CaetanoVidal <calmgv@gmail.com>
2025-07-04 14:09:50 -0300

    pdf or image function

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import easyocr
+
+
+READER = easyocr.Reader('en') # this needs to run only once to load the model into memory
+
+
+def pdf_or_image(file_path):
+    valid_image_exts = ('.png', '.jpg', '.jpeg', '.bmp', '.tiff')
+    file_ext = os.path.splitext(file_path)[1].lower()
+
+    if file_ext in valid_image_exts:
+        return "image"
+    elif file_ext == '.pdf':
+        return "pdf"
+
+    raise ValueError(f"Unsupported file format: '{file_ext}'. Only images and PDFs are allowed.")
+
+
+path_img = "documents/the human brain image.png"
+path_pdf = "documents/Caetano CV.pdf"
+
+def read_image(file_path):
+    text = READER.readtext('chinese.jpg', detail = 0)
+    return text
+
+
+def read_pdf(file_path):
+    print('in dev')
+
+path = path_img
+
+
+
+if pdf_or_image(path) == 'image':
+    t = read_image(path)
+    print(t)
+else pdf_or_image(path) == 'pdf':
+    t = read_pdf(file_path)
